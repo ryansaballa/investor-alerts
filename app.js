55922d3c4592fe760cc301af44295adfa0e3c109
ryansaballa <154952941+ryansaballa@users.noreply.github.com>
2026-05-14 17:12:55 -0400

    included sort method for expired alerts
    alerts beyond 2 days (createdAt) will show up in admin

--- app.js
+++ app.js
@@ -50,39 +50,68 @@ function setupForm() {
     });
 }
 
+// adds interactivity when user clicks on the approve button, but only updates the array in the local storage from 'pending' to 'approved'
+
+function approveAlert(id) {
+    const alerts = getAlerts();
+
+    const updatedAlerts = alerts.map((alert) => {
+        // displays each alert
+        if (alert.id === Number(id)) {
+            // finds the alert that the user clicked
+            return {
+                ...alert,
+                status: "approved",
+                publishedAt: formatDate(alert.createdAt),
+            };
+        }
+        // displays the approved alert
+        return alert;
+    });
+
+    saveAlerts(updatedAlerts);
+    loadAdminAlerts();
+}
+
 // admin dashboard interaction
 
 function loadAdminAlerts() {
     const container = document.getElementById("adminContainer");
+    const overdueContainer = document.getElementById("adminContainerExp");
     const alerts = getAlerts();
 
-    // admin only sees 'pending' alerts
-    const pendingAlerts = alerts.filter((a) => a.status === "pending");
+    // admin only sees 'pending' alerts and filters non-expired alerts and sorted by risk level
+    const pendingAlerts = alerts.filter((a) => a.status === "pending" && !expiredAlerts(a.createdAt)).sort((a, b) => riskOrder[b.riskLevel] - riskOrder[a.riskLevel]);
+
+    // admin sees expired alerts
+    const overdueAlerts = alerts
+        .filter((a) => a.status === "approved" && a.createdAt && expiredAlerts(a.createdAt))
+        .sort((a, b) => riskOrder[b.riskLevel] - riskOrder[a.riskLevel]);
 
     container.innerHTML = "";
+    overdueContainer.innerHTML = "";
 
     if (pendingAlerts.length === 0) {
-        container.innerHTML = "<p>No alerts available.</p>";
-        return;
+        container.innerHTML = '<div class="alertCard"><p>No alerts available.</p></div>';
     }
 
     pendingAlerts.forEach((alert) => {
         const div = document.createElement("div");
-        div.classList.add("alert-card");
+        div.classList.add("alertCard");
 
         div.innerHTML = `
     <h3>${alert.title}</h3>
 
-  <p><strong>Date Created:</strong> ${alert.createdAt}</p>
+  <p><strong>Date Created:</strong> ${formatDate(alert.createdAt)}</p>
   <p><strong>Status:</strong> ${alert.status}</p>
-  ${alert.publishedAt ? `<p><strong>Published:</strong> ${new Date(alert.publishedAt).toLocaleString()}</p>` : ""}
+  ${alert.publishedAt ? `<p><strong>Published:</strong> ${alert.publishedAt}</p>` : ""}
 
   <p><strong>Risk Level:</strong> ${alert.riskLevel}</p>
 
   <p><strong>Summary:</strong><br>${alert.summary}</p>
 
   <p>
-    <a href="${alert.link}" target="_blank">View Source</a>
+    Related Link: <a href="${alert.link}" target="_blank">${alert.title}</a>
   </p>
   ${
       alert.status === "pending"
@@ -96,32 +125,40 @@ function loadAdminAlerts() {
     `;
         container.appendChild(div);
     });
-}
 
-// adds interactivity when user clicks on the approve button, but only updates the array in the local storage from 'pending' to 'approved'
+    overdueAlerts.forEach((alert) => {
+        const div = document.createElement("div");
+        div.classList.add("alertCard");
 
-function approveAlert(id) {
-    const alerts = getAlerts();
+        div.innerHTML = `
+    <h3>${alert.title}</h3>
 
-    const updatedAlerts = alerts.map((alert) => {
-        // displays each alert
-        if (alert.id === Number(id)) {
-            // finds the alert that the user clicked
-            return {
-                ...alert,
-                status: "approved",
-                publishedAt: new Date().toISOString(),
-            };
-        }
-        // displays the approved alert
-        return alert;
+  <p><strong>Date Created:</strong> ${formatDate(alert.createdAt)}</p>
+  <p><strong>Status:</strong> ${alert.status}</p>
+  ${alert.publishedAt ? `<p><strong>Published:</strong> ${alert.publishedAt}</p>` : ""}
+
+  <p><strong>Risk Level:</strong> ${alert.riskLevel}</p>
+
+  <p><strong>Summary:</strong><br>${alert.summary}</p>
+
+  <p>
+    Related Link: <a href="${alert.link}" target="_blank">${alert.title}</a>
+  </p>
+  ${
+      alert.status === "pending"
+          ? `
+      <button onclick="approveAlert(${alert.id})">Approve</button>
+      <button onclick="rejectAlert(${alert.id})">Reject</button>
+    `
+          : ""
+  }
+    
+    `;
+        overdueContainer.appendChild(div);
     });
-
-    saveAlerts(updatedAlerts);
-    loadAdminAlerts();
 }
 
-// rending alerts - rejected (optional)
+// rendering alerts - rejected (optional)
 
 function rejectAlert(id) {
     const alerts = getAlerts();
@@ -144,18 +181,22 @@ function rejectAlert(id) {
 
 function loadPublicAlerts() {
     const container = document.getElementById("alertContainer");
+
     const alerts = getAlerts();
-    const approved = alerts.filter((a) => a.status === "approved");
+
+    // filters the list of rendered alerts from newest to oldest
+    const approved = alerts.filter((a) => a.status === "approved").sort((a, b) => new Date(b.publishedAt) - new Date(a.publishedAt));
+
     container.innerHTML = "";
 
     approved.forEach((alert) => {
         const div = document.createElement("div");
         div.classList.add("alertCard");
         div.innerHTML = `
-		<h3>${alert.title}</h3>
 		<p>${formatDate(alert.publishedAt)}</p>
-    <p>${alert.summary}</p>
-    <a href="${alert.link}" target="_blank">Source</a>
+    <h3>${alert.title}</h3>
+		<p>${alert.summary}</p>
+    <a href="${alert.link}" target="_blank">Related Link</a>
 		`;
         container.appendChild(div);
     });
@@ -168,6 +209,34 @@ function resetAlerts() {
     loadAdminAlerts(); // refresh UI
 }
 
+// expiration for alerts of 2 days passed created Date
+
+function expiredAlerts(dateString) {
+    const today = new Date();
+    const createdDate = new Date(dateString);
+
+    today.setHours(0, 0, 0, 0);
+    createdDate.setHours(0, 0, 0, 0);
+
+    const diffTime = today - createdDate;
+    const diffDays = diffTime / (1000 * 60 * 60 * 24); // converts difference in diffDays
+
+    return diffDays > 2;
+}
+
+// deletion function for expired alerts
+function deleteExpiredAlerts() {
+    const alerts = getAlerts();
+    const updatedAlerts = alerts.filter((alert) => {
+        return !expiredAlerts(alert.createdAt); // keeps not expired alerts
+    });
+
+    saveAlerts(updatedAlerts);
+
+    loadAdminAlerts();
+    loadPublicAlerts();
+}
+
 // formats the date as Month - Day - Year
 function formatDate(dateString) {
     return new Date(dateString).toLocaleDateString("en-US", {
@@ -177,4 +246,10 @@ function formatDate(dateString) {
     });
 }
 
-// expiration for alerts
+// formats alerts by risk Level
+
+const riskOrder = {
+    high: 3,
+    medium: 2,
+    low: 1,
+};
